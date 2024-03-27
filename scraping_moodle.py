dc026eedc4624022b6ec4bdb9aeec0547beab74d
giuliapetrilli <236888@students.hertie-school.org>
2024-03-27 15:26:20 +0100

    scraping oag

--- scraping_moodle.py
+++ scraping_moodle.py
@@ -18,30 +18,33 @@ password = "Hertie!!!2"
 # Open the connect login page
 driver.get(connect_login_url)
 
-time.sleep(3)
+#time.sleep(3)
 
 # Fill in the username and password
 #username_field = driver.find_element(By.CLASS_NAME, "text span4 extralarge")
 username_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Email-address or username']")
 
 #password_field = driver.find_element(By.CLASS_NAME, "password span4 extralarge")
-password_field = driver.find_element(By.CSS_SELECTOR, ".password.span4.extralarge")
+password_field = driver.find_element(By.NAME, "loginpwd")
 
-time.sleep(3)
-username_field.send_keys(username + Keys.ENTER)
-time.sleep(3)
+#time.sleep(3)
+#username_field.send_keys(username + Keys.ENTER)
+username_field.send_keys(username)
+#time.sleep(3)
 #password_field.send_keys(password + Keys.ENTER)
+password_field.send_keys(password)
 time.sleep(3)
 
 # Click the login button - adjust the selector 
-#login_button = driver.find_element(By.LINK_TEXT, "Login")
-#login_button.click()
+login_button = driver.find_element(By.LINK_TEXT, "Login")
+login_button.click()
+time.sleep(10)
 
 # pause login process to complete
 #time.sleep(10)
 
 # navigating to the members page 
-#driver.get("https://connect.hertie-school.org/directory/")
+driver.get("https://connect.hertie-school.org/contact/")
 
 # actual scraping starts here
 
@@ -51,5 +54,4 @@ time.sleep(3)
 
 # Remember to quit the driver session when you're done
 #time.sleep(10)
-#driver.quit()
-
+driver.quit()
